8ee3d2be9b1a47a69b2e6f2528a415e9143a970d
RGu0000 <ryszardgurzeda@gmail.com>
2018-04-12 20:36:35 +0200

    Lesson 3 done

---
+++ spec/exercises/Lesson2_1_spec.rb
@@ -0,0 +1,5 @@
+require './exercises/Lesson2_1'
+
+context "example1" do
+  it { expect(solution([9, 3, 9, 3, 9, 7, 9], 0)).to eq [9, 3, 9, 3, 9, 7, 9] }
+end
---
+++ spec/exercises/Lesson3_spec.rb
@@ -0,0 +1,5 @@
+require './exercises/Lesson3'
+
+context "example1" do
+  it { expect(solution([3, 1, 2, 4, 3])).to eq 1 }
+end
