ed69742cadf4395872acc2d0b0515167c0c07ca4
RGu0000 <ryszardgurzeda@gmail.com>
2018-04-25 22:42:28 +0200

    simple numbers

--- exercises/Playground/Blocks.rb
+++ exercises/Playground/Blocks.rb
@@ -3,3 +3,17 @@ def two_times
   yield
 end
 two_times { puts "Hello" }
+
+
+p enum_in_threes = (1..10).enum_for(:each_slice, 3)
+p enum_in_threes.to_a
+
+p 123456
+p 0d123456
+p 123_456
+p -543
+p 0xaabb
+p 0377
+p -0b10_1010
+p 0b10_1010
+p 123_456_789_123_456_789
